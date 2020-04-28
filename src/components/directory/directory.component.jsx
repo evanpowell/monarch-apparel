0878083800dedd52017640e4875e0f1092618d7e
evanpowell <evanpowell87@gmail.com>
2020-04-28 00:49:00 -0400

    create components

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './directory.styles.scss';
+
+export class Directory extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Directory;
---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MenuItem;
