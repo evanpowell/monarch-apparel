e284793b4e834a473a01fbb8e101676f09f0214b
evanpowell <evanpowell87@gmail.com>
2020-04-28 13:55:02 -0400

    apply hover animations

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,10 +3,11 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => {
     return (
-        <div className={`menu-item ${size ? 'menu-item--' + size : ''}`}
-            style={{
-                backgroundImage: `url(${imageUrl})`
-            }}>
+        <div className={`menu-item ${size ? 'menu-item--' + size : ''}`}>
+            <div className="menu-item--background"
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}></div>
             <div className="menu-item__content">
                 <h1 className="menu-item__title">{title}</h1>
                 <span className="menu-item__subtitle">SHOP NOW</span>
