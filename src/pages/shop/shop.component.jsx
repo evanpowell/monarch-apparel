f7ca787209777739a1bac50e8e9dd3b0a7e7dbf6
evanpowell <evanpowell87@gmail.com>
2020-04-29 13:53:15 -0400

    implement shop styles

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
+import './shop.styles.scss';
 
 import SHOP_DATA from './shop.data';
+
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 export class ShopPage extends Component {
     constructor(props) {
         super(props)
     
         this.state = {
-            shopData: SHOP_DATA
+            collections: SHOP_DATA
         }
     }
     
     render() {
+        const { collections } = this.state;
         return (
-            <div>
-                
+            <div className="shop-page">
+                {
+                    collections.map(({ id, ...otherCollectionProps }) => (
+                        <CollectionPreview key={id} { ...otherCollectionProps }></CollectionPreview>
+                    ))
+                }
             </div>
         )
     }
