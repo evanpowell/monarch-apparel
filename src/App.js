0c0a8f4c6a973671598c869ea31e5dacb86bf6df
evanpowell <evanpowell87@gmail.com>
2020-05-01 22:35:40 -0400

    implement signout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.scss';
 
@@ -7,19 +7,47 @@ import HomePage from './pages/home/home.component';
 import ShopPage from './pages/shop/shop.component';
 import LoginPage from './pages/login/login.component';
 import SignUpPage from './pages/sign-up/sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-    return (
-        <div>
-            <Header />
-            <Switch>
-                <Route exact path='/' component={HomePage}></Route>
-                <Route path='/shop' component={ShopPage}></Route>
-                <Route path='/login' component={LoginPage}></Route>
-                <Route path='/sign-up' component={SignUpPage}></Route>
-            </Switch>
-        </div>
-    );
+
+export class App extends Component {
+    constructor() {
+        super()
+    
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+            this.setState({
+                currentUser: user
+            });
+
+            console.log(user);
+        });
+    }
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+    
+    render() {
+        return (
+            <div>
+                <Header currentUser={this.state.currentUser} />
+                <Switch>
+                    <Route exact path='/' component={HomePage}></Route>
+                    <Route path='/shop' component={ShopPage}></Route>
+                    <Route path='/login' component={LoginPage}></Route>
+                    <Route path='/sign-up' component={SignUpPage}></Route>
+                </Switch>
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,29 +1,49 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-
-import { ReactComponent as Logo } from '../../assets/logo.svg';
-
 import classes from './header.module.scss';
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+import { auth } from '../../firebase/firebase.utils';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
     return (
         <div className={classes[`header`]}>
             <Link className={classes[`logo-container`]} to="/">
                 <Logo />
             </Link>
             <div className={classes[`nav-options`]}>
+                {
+                    currentUser ? 
+                    (
+                        <div className={`${classes[`option`]} ${classes[`option--user`]}`}>
+                            { currentUser.displayName || currentUser.email }
+                        </div>
+                    ) :
+                    null
+                }
                 <Link className={classes[`option`]} to="/shop">
                     SHOP
                 </Link>
                 <Link className={classes[`option`]} to="/contact">
                     CONTACT
                 </Link>
-                <Link className={classes[`option`]} to="/login">
-                    LOGIN
-                </Link>
-                <Link className={classes[`option`]} to="/sign-up">
-                    SIGN UP
-                </Link>
+                { currentUser ? 
+                    (
+                        <Fragment>
+                            
+                            <div className={classes[`option`]} onClick={() => auth.signOut()}>SIGN OUT</div>
+                        </Fragment>
+                    ) :
+                    (
+                        <Fragment>
+                            <Link className={classes[`option`]} to="/login">
+                                LOGIN
+                            </Link>
+                            <Link className={classes[`option`]} to="/sign-up">
+                                SIGN UP
+                            </Link>
+                        </Fragment>
+                    )
+                }
             </div>
         </div>
     )
