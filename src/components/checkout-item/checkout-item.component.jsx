02c1fdd1702012bd91d0943983d890c60fd374a4
evanpowell <evanpowell87@gmail.com>
2020-05-14 11:43:49 -0400

    implement remove cart item

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { cartActions } from '../../redux/modules/cart';
 
 import classes from './checkout-item.module.scss';
 
-const CheckoutItem = ({cartItem: { imageUrl, name, price, quantity }}) => {
+const CheckoutItem = ({ cartItem, removeCartItem }) => {
+    const { imageUrl, name, quantity, price, id } = cartItem;
     return (
         <div className={classes[`checkout-item`]}>
             <div className={classes[`checkout-item__content`]}>
@@ -11,15 +15,19 @@ const CheckoutItem = ({cartItem: { imageUrl, name, price, quantity }}) => {
             <span className={classes[`checkout-item__content`]}>{ name }</span>
             <span className={classes[`checkout-item__content`]}>{ quantity }</span>
             <span className={classes[`checkout-item__content`]}>{ price }</span>
-            <button className={classes[`checkout-item__content`]} aria-label="remove item">
+            <button
+                className={`${classes[`checkout-item__content`]} clear-button`}
+                aria-label="remove item"
+                onClick={() => removeCartItem(id)}
+            >
                 &#10005;
             </button>
         </div>
     );
 };
 
-// const mapDispatchToProps = (dispatch) => {
-    
-// }
+const mapDispatchToProps = (dispatch) => ({
+    removeCartItem: (cartItemId) => dispatch(cartActions.removeCartItem(cartItemId))
+});
 
-export default CheckoutItem;
+export default connect(null, mapDispatchToProps)(CheckoutItem);
