8f4e8f38729352f9cbd4c7fb8597761b5e9d7371
evanpowell <evanpowell87@gmail.com>
2020-05-14 23:02:24 -0400

    persist cart items to local storage

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
+
 import App from './App';
-import store from './redux/configureStore';
+import { store, persistor } from './redux/configureStore'; 
+
 import './styles/main.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,35 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './modules/user';
 import cartReducer from './modules/cart';
 
 const middleware = [logger];
 
-const rootReducer = combineReducers({
-    user: userReducer,
-    cart: cartReducer
-});
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+};
+
+const rootReducer = persistReducer(
+    persistConfig,
+    combineReducers({
+        user: userReducer,
+        cart: cartReducer
+    })
+);
 
 const store = createStore(
     rootReducer,
     applyMiddleware(...middleware)
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export {
+    store,
+    persistor
+};
